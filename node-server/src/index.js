009e52c3e6b5c5cf3a1bde00da4257ac1beeb49c
MoniqueOg <moniqueogunk@icloud.com>
2023-01-27 13:08:17 +0000

    add/feat node.js/html/txt file

--- node-server/src/index.js
+++ node-server/src/index.js
@@ -1,4 +1,2 @@
-const fs = require('fs');
-
 const content = fs.readFileSync('textfile.txt','utf8');
 console.info(content);
--- static-file-server/src/index.js
+++ static-file-server/src/index.js
@@ -1,1 +1,16 @@
 /* Add your server code here*/
+var http = require("http"),
+  fs = require("fs");
+
+  var server = http
+    .createServer(function (request, response) {
+      fs.readFile("./index.html", function (err, html) {
+        if (err) {
+          throw err;
+        }
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
+      });
+    })
+    .listen(8080);
